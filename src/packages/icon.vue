5fd26f9dd79e53248cc0d53cdeedbf1571c445e3
cj <yuchangjiang720@163.com>
2022-07-08 10:10:18 +0800

    add button components

---
+++ src/packages/icon.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg class="poly-icon" aria-hidden="true">
+    <use :xlink:href="`#${icon}`" />
+  </svg>
+</template>
+
+<script>
+import './../style/icon'
+export default {
+  name: 'vking-icon',
+  props:{
+    icon:{
+      type:String,
+      require:true // 设置必传
+    }
+  }
+}
+</script>
+
+<style>
+.poly-icon {
+  width: 30px;
+  height: 30px;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
